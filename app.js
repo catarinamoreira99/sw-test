1031f023a99a8f7c0758c0efb1db940409aa29ec
catarinamoreira99 <31781640+catarinamoreira99@users.noreply.github.com>
2022-03-10 21:28:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js', {scope:'./sw-test/'})
+    navigator.serviceWorker.register('./sw.js', {scope:'./'})
     .then((reg) => {
         console.log('Registration succeeded. Scope is ' +reg.scope);
 
